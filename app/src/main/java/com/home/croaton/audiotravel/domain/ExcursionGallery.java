bed0f0953cee7f70df40734b5fb01d84a75b7b25
StanislavKhalash <stanislav.khalash@gmail.com>
2016-04-03 23:41:30 +0200

    Fixed hardcoded excursion

--- app/src/main/java/com/home/croaton/audiotravel/domain/ExcursionGallery.java
+++ app/src/main/java/com/home/croaton/audiotravel/domain/ExcursionGallery.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 public class ExcursionGallery {
     public ArrayList<ExcursionBrief> getAvailableExcursions(){
         ArrayList<ExcursionBrief> excursions = new ArrayList<ExcursionBrief>();
-        excursions.add(new ExcursionBrief(UUID.randomUUID(), "Gamlastan", "gamlastan"));
+        excursions.add(new ExcursionBrief(UUID.randomUUID(), "Demo", "gamlastan"));
         excursions.add(new ExcursionBrief(UUID.randomUUID(), "Abrahamsberg", "abrahamsberg"));
         excursions.add(new ExcursionBrief(UUID.randomUUID(), "Gamlastan", "gamlastan"));
 
